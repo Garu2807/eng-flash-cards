8e309d2edcb342674585720755b8df495a1083c7
Garu2807 <arutyunyangagik@gmail.com>
2023-06-29 21:45:09 +0300

    add flipping cards

--- components/pages/Layout.jsx
+++ components/pages/Layout.jsx
@@ -10,19 +10,23 @@ function Layout({ title, children, user }) {
         <link rel="stylesheet" href="/css/style.css" />
         <meta charSet="UTF-8" />
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
           crossOrigin="anonymous"
         />
-        <link rel="stylesheet" href='/css/bootstrap.min.css'/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="stylesheet" href="/css/style.css" />
         <script defer src="/js/auth.js" />
       </head>
       <body>
         <Header user={user} />
-        {children}
+        <div className="container">{children}</div>
       </body>
     </html>
   );
--- components/pages/TopicPage.jsx
+++ components/pages/TopicPage.jsx
@@ -1,26 +1,24 @@
-const React = require('react')
-const Layout = require("./Layout");
+const React = require('react');
+const Layout = require('./Layout');
 
 function TopicPage({ topics, user }) {
-    return (
-        <Layout user={user}>
-            <div className="container">
-                <h2> Выбери тему</h2>
-                <div>
-                    <div id="topic-container">
-                        {topics.map((topic) => (
-                            <div>
-                                <a href={`/topic/${topic.id}/cards`}>
-                                    <h2>{topic.topic}</h2>
-                                </a>
-                            </div>
-                        ))}
+  return (
+    <Layout user={user}>
+      <div className="container">
+        <h2 id="topic-select"> Выбери тему</h2>
 
-                    </div>
-                </div>
+        <div id="topic-container">
+          {topics.map((topic) => (
+            <div className="card-back">
+              <a href={`/topic/${topic.id}/cards`}>
+                <h2>{topic.topic}</h2>
+              </a>
             </div>
-        </Layout>
-    );
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 module.exports = TopicPage;
